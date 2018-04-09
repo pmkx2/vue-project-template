014dd34fe17e85e3eca4acb86862691e0f6d81a7
LuoYongjiang <185832959@qq.com>
2018-04-09 17:02:54 +0800

    upgrade css-loader

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import 'vue-svgicon/dist/polyfill'
 import Vue from 'vue'
 import router from './router'
 import store from 'store'
-import meta from 'vue-meta'
 
-// Vue.config.productionTip = false
+import 'normalize.css/normalize.css'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import 'src/style/app.scss'
 import App from 'src/views/app'
@@ -14,10 +16,6 @@ import * as svgicon from 'vue-svgicon'
 import './views/icons'
 Vue.use(svgicon, {
     tagName: 'icon'
-})
-
-Vue.use(meta, {
-    keyName: 'metaInfo'
 })
 
 /* eslint-disable no-new */
