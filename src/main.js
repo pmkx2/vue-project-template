8c1fdd21b5fc1f3881c03217b52aa7c6a13460d9
LuoYongjiang <185832959@qq.com>
2018-07-19 17:06:06 +0800

    remove err

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import 'babel-polyfill'
 import 'vue-svgicon/dist/polyfill'
 import Vue from 'vue'
 
-import { vueHandleErr } from 'common/err'
 import 'normalize.css/normalize.css'
 import 'ui/element'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -19,8 +18,6 @@ import './views/icons'
 Vue.use(svgicon, {
     tagName: 'icon'
 })
-
-Vue.config.errorHandler = vueHandleErr
 
 /* eslint-disable no-new */
 new Vue({
